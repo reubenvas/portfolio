d7fb34cfeaf7f018ff423dbcac8e8dee5bb58572
Reuben <reuben.vas@hotmail.com>
2019-04-22 14:21:16 +0200

    Updates

---
+++ precache-manifest.7ea3fcc5e152d96147bc382fd64f19b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ca33563b179a9d61ab0d76f39b5466e",
+    "url": "/portfolio/static/media/preggy.0ca33563.png"
+  },
+  {
+    "revision": "218a73e3d0417caea30fe39da5f784cf",
+    "url": "/portfolio/static/media/park_away.218a73e3.PNG"
+  },
+  {
+    "revision": "948858f551a29aec82172731567c48a3",
+    "url": "/portfolio/static/media/cool-background.948858f5.png"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "54505072af388438cae7",
+    "url": "/portfolio/static/js/main.169cc253.chunk.js"
+  },
+  {
+    "revision": "ad3c0ffd1d3b21c1e4b8",
+    "url": "/portfolio/static/js/2.4473cbac.chunk.js"
+  },
+  {
+    "revision": "54505072af388438cae7",
+    "url": "/portfolio/static/css/main.1e11e69b.chunk.css"
+  },
+  {
+    "revision": "021d3bc74782f6ef3b8abb855ca25e27",
+    "url": "/portfolio/index.html"
+  }
+];
