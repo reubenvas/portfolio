a0e1e6102f944e967686633874f40c030fd19a44
Reuben <reuben.vas@hotmail.com>
2019-04-22 14:30:02 +0200

    Updates

---
+++ precache-manifest.f3f9ce92b26533af4c62dd66dab3e14d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ca33563b179a9d61ab0d76f39b5466e",
+    "url": "/static/media/preggy.0ca33563.png"
+  },
+  {
+    "revision": "218a73e3d0417caea30fe39da5f784cf",
+    "url": "/static/media/park_away.218a73e3.PNG"
+  },
+  {
+    "revision": "948858f551a29aec82172731567c48a3",
+    "url": "/static/media/cool-background.948858f5.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d91e62ae4ace7a43dcb9",
+    "url": "/static/js/main.06d21a79.chunk.js"
+  },
+  {
+    "revision": "ad3c0ffd1d3b21c1e4b8",
+    "url": "/static/js/2.4473cbac.chunk.js"
+  },
+  {
+    "revision": "d91e62ae4ace7a43dcb9",
+    "url": "/static/css/main.09d48bf6.chunk.css"
+  },
+  {
+    "revision": "c0385ebc58c057a124715b6f1a3ef8f9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7ea3fcc5e152d96147bc382fd64f19b3.js"
+  "/precache-manifest.f3f9ce92b26533af4c62dd66dab3e14d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
